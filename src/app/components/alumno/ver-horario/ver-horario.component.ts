df1b8e4e71b585ab62168fd3903faf80e35d4587
JM02022 <018101636j@uandina.edu.pe>
2022-06-22 14:11:57 -0500

    2022/06/22

--- src/app/components/alumno/ver-horario/ver-horario.component.ts
+++ src/app/components/alumno/ver-horario/ver-horario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from 'src/app/models/curso';
+import { CursoDetalle } from 'src/app/models/cursoDetalle';
 import { Nota } from 'src/app/models/nota';
+import { CursoDetalleService } from 'src/app/services/cursos-detalles/curso-detalle.service';
 import { CursoService } from 'src/app/services/cursos/curso.service';
 import { NotaService } from 'src/app/services/notas/nota.service';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
@@ -12,11 +14,20 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 })
 export class VerHorarioComponent implements OnInit {
   public cursos:Curso[] = []; 
-  public notas: Nota[] = [];
-  constructor(private usuario:UsuarioService,private apiCurso: CursoService,private notaApi: NotaService) { }
-
+  public Usuario = {
+    usuario: "",
+    rol: "",
+    sexo: "",
+    datos: []
+  }
+  constructor(
+    private usuario:UsuarioService,
+    private apiCurso: CursoService) { }
+  
   ngOnInit(): void {
-    
+    this.apiCurso.getCursos().subscribe(data=>{
+      this.cursos = data
+    })  
   }
   
 
